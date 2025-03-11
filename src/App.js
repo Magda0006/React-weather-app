3afc8e6a70870fb9aed636e1a64e83480f3bd52d
Magda0006 <lenamagdamaria@gmail.com>
2025-03-11 16:40:43 +0000

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import "./App.css";
 import Weather from "./Weather";
+
 function App() {
   return (
-    <div className="App">
-      <div className="container m-5">
-        <Weather />
-        <footer>
-          This project was coded by Magda Tudora and it is open sourced on{" "}
-          <a
-            href="https://github.com/Magda0006/React-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-        </footer>
-      </div>
+    <div className="App container-fluid ">
+      <Weather />
+      <footer>
+        This project was coded by Magda Tudora and it is open sourced on{" "}
+        <a
+          href="https://github.com/Magda0006/React-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,10 +2,21 @@ import React from "react";
 import "./Weather.css";
 export default function Weather() {
   return (
-    <div className="weather p-3">
-      <form className="weather-search">
-        <input type="search" placeholder="Enter a city.." />
-        <input type="submit" className="btn btn-primary ms-2" />
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-sm-9 ">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control w-100"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-sm-3 ">
+            <input type="submit" className="btn btn-primary ms-2 w-100" />
+          </div>
+        </div>{" "}
       </form>
       <h1>Rome</h1>
       <ul>
@@ -18,7 +29,8 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly cloudy"
           />{" "}
-          18 C°
+          <span className="temperature">18</span>{" "}
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
